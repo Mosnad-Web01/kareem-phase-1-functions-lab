81f6887b1cba01fde5e35f47e5d99c77a385e5f9
kareemAlqubati <salamkareem792@gmail.com>
2024-08-22 15:07:36 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+  const headquartersBlock = 42;
+
+  const distanceInBlocks = Math.abs(someValue - headquartersBlock);
+
+  return distanceInBlocks;
+}
+
+
+
+function distanceFromHqInFeet(someValue) {
+  const blocks = distanceFromHqInBlocks(someValue);
+
+  const feet = blocks * 264;
+
+  return feet;
+}
+
+function distanceTravelledInFeet(start, destination) {
+
+  const blocksTraveled = Math.abs(destination - start);
+
+  const feet = blocksTraveled * 264;
+
+  return feet;
+}
+
+function calculatesFarePrice(start, destination) {
+  const distanceInFeet = distanceTravelledInFeet(start, destination);
+
+  if (distanceInFeet > 2500) {
+    return "cannot travel that far";
+  }
+
+  if (distanceInFeet <= 400) {
+    return 0;
+  } else if (distanceInFeet <= 2000) {
+    return (distanceInFeet - 400) * 0.02;
+  } else {
+    return 25;
+  }
+}
